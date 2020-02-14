b78636a69b9d453a68c9f4ba57785b73a53a51fa
Muhammad Shariq <49516795+shariqahmed123@users.noreply.github.com>
2020-02-14 23:01:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function menuToggle(){
+    let nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    let toggle = document.getElementById('toggle');
+    toggle.classList.toggle('active');
+}
